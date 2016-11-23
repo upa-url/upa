1aa4459214848fe0b5c7d8e49af248cd8e7f3ecd
Rimas Misevičius <rmisev3@gmail.com>
2016-11-23 17:51:01 +0200

    testas su L"..." (wchar_t)

--- test.cpp
+++ test.cpp
@@ -17,7 +17,11 @@ void url_parse(const CharT* str_url, whatwg::url* base = nullptr)
     
     whatwg::url url;
 
-    std::cout << str_url << "\n";
+    if (sizeof(CharT) == 1)
+        std::cout << str_url;
+    else
+        std::wcout << str_url;
+    std::cout << "\n";
     url.parse(str_url, base);
     for (int part = whatwg::url::SCHEME; part < whatwg::url::PART_COUNT; part++) {
         std::string strPart = url.get_part(static_cast<whatwg::url::PartType>(part));
@@ -36,7 +40,7 @@ int main()
 
     url_parse("mailto:vardenis@example.com", nullptr);
 
-    url_parse((const char16_t*)L"http://klausimėlis.lt/?key=ąče#axd");
+    url_parse(L"http://klausimėlis.lt/?key=ąče#axd");
     url_parse("https://username:pass@word@example.com:123/path/data?abc=ąbč&key=value&key2=value2#fragid1-ą");
 
     url_parse("   wss\r:\n/\t/abc.lt/ \t ", nullptr);
