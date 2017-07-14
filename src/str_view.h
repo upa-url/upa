41827727b82312dff83afe866b8ea17a802186cd
Rimas Misevičius <rmisev3@gmail.com>
2017-07-15 00:33:04 +0300

    Make str_view to be closer to basic_string_view
    - add types, default constructors & assigment
    - empty(), swap(x)

--- src/str_view.h
+++ src/str_view.h
@@ -9,40 +9,72 @@
 template<typename CharT, typename Traits = std::char_traits<CharT>>
 class str_view {
 public:
-    str_view(const str_view& sv) : ptr_(sv.ptr_), len_(sv.len_) {}
+    // types
+    using traits_type = Traits;
+    using value_type = CharT;
+    using pointer = CharT*;
+    using const_pointer = const CharT*;
+    using reference = CharT&;
+    using const_reference = const CharT&;
+    using const_iterator = pointer; // implementation-defined; see 21.4.2.2
+    using iterator = const_iterator;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using reverse_iterator = const_reverse_iterator;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    // static constexpr size_type npos = size_type(-1);
+
+    // construction and assignment
+    str_view() : ptr_(nullptr), len_(0) {}
+    str_view(const str_view&) = default;
+    str_view& operator=(const str_view&) = default;
     str_view(const CharT* ptr, std::size_t len) : ptr_(ptr), len_(len) {}
     str_view(const CharT* ptr) : ptr_(ptr), len_(Traits::length(ptr)) {}
     str_view(const std::basic_string<CharT>& str) : ptr_(str.data()), len_(str.length()) {}
 
-    const CharT* data() const { return ptr_; }
-    std::size_t length() const { return len_; }
-    std::size_t size() const { return len_; }
+    // iterator support
     const CharT* begin() const { return ptr_; }
     const CharT* end() const { return ptr_ + len_; }
 
-    CharT operator[](std::size_t ind) const {
+    // capacity
+    std::size_t size() const { return len_; }
+    std::size_t length() const { return len_; }
+    bool empty() const { return len_ == 0; }
+
+    // element access
+    const CharT& operator[](std::size_t ind) const {
         return ptr_[ind];
     }
+    const CharT* data() const { return ptr_; }
 
-    int compare(str_view x) const {
-        const int cmp = Traits::compare(ptr_, x.ptr_, std::min(len_, x.len_));
-        return cmp != 0 ? cmp : (len_ == x.len_ ? 0 : len_ < x.len_ ? -1 : 1);
-    }
-
-    bool equal(str_view x) const {
-        return len_ == x.len_ && Traits::compare(ptr_, x.ptr_, len_) == 0;
-    }
-
-    void append_to(std::basic_string<CharT>& str) const {
-        str.append(ptr_, len_);
-    }
-
+    // modifiers
     void remove_prefix(size_t n) {
         ptr_ += n;
         len_ -= n;
     }
     void remove_suffix(size_t n) {
         len_ -= n;
+    }
+    void swap(str_view& x) {
+        const str_view tmp{x};
+        x = *this;
+        *this = tmp;
+    }
+
+    // string operations
+    int compare(str_view x) const {
+        const int cmp = Traits::compare(ptr_, x.ptr_, std::min(len_, x.len_));
+        return cmp != 0 ? cmp : (len_ == x.len_ ? 0 : len_ < x.len_ ? -1 : 1);
+    }
+
+
+    // non-standart
+    bool equal(str_view x) const {
+        return len_ == x.len_ && Traits::compare(ptr_, x.ptr_, len_) == 0;
+    }
+
+    void append_to(std::basic_string<CharT>& str) const {
+        str.append(ptr_, len_);
     }
 
 private:
