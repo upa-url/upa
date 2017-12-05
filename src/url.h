b767adb9b7be6b7373c33c0042a5690c8693efd0
Rimas Misevičius <rmisev3@gmail.com>
2017-12-05 23:06:45 +0200

    Add escaped_dot lambda and use it in the double_dot and single_dot

--- src/url.h
+++ src/url.h
@@ -1714,22 +1714,28 @@ inline void url_parser::parse_path(url_serializer& urls, const CharT* first, con
     // path state; includes:
     // 1. [ (/,\) - 1, 2, 3, 4 - [ 1 (if first segment), 2 ] ]
     // 2. [ 1 ... 4 ]
-    auto double_dot = [](const CharT* const pointer, const std::size_t len) -> bool {
+    static const auto escaped_dot = [](const CharT* const pointer) -> bool {
+        return pointer[0] == '%' && pointer[1] == '2' &&
+            (pointer[2] == 'e' || pointer[2] == 'E');
+    };
+    const auto double_dot = [](const CharT* const pointer, const std::size_t len) -> bool {
         if (len == 2) {
             return (pointer[0] == '.' && pointer[1] == '.');
         } else if (len == 4) {
-            return detail::AsciiEqualNoCase(pointer, pointer + 4, ".%2e") ||
-                   detail::AsciiEqualNoCase(pointer, pointer + 4, "%2e.");
+            // ".%2e" or "%2e."
+            return (pointer[0] == '.' && escaped_dot(pointer + 1)) ||
+                (escaped_dot(pointer) && pointer[3] == '.');
         } else if (len == 6) {
-            return detail::AsciiEqualNoCase(pointer, pointer + 6, "%2e%2e");
+            // "%2e%2e"
+            return escaped_dot(pointer) && escaped_dot(pointer + 3);
         } else
             return false;
     };
-    auto single_dot = [](const CharT* const pointer, const std::size_t len) -> bool {
+    const auto single_dot = [](const CharT* const pointer, const std::size_t len) -> bool {
         if (len == 1) {
             return pointer[0] == '.';
         } else if (len == 3) {
-            return detail::AsciiEqualNoCase(pointer, pointer + 3, "%2e");
+            return escaped_dot(pointer); // "%2e"
         } else
             return false;
     };
