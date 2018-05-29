b88de7a6b751e5b75d4dbc5fe7598ff5a1c4cc37
Rimas Misevičius <rmisev3@gmail.com>
2018-05-29 23:37:11 +0300

    Add commands into clean_vc.bat to delete VS 2017 working files

--- msvc/test_sample/clean_vc.bat
+++ msvc/test_sample/clean_vc.bat
@@ -9,3 +9,8 @@ del %p%\Debug\*.log
 del %p%\Release\*.log
 for /D %%s in (%p%\Debug\*.tlog) do rmdir /S /Q %%s
 for /D %%s in (%p%\Release\*.tlog) do rmdir /S /Q %%s
+REM VS 2017
+for /D %%s in (%p%\.vs\*) do (
+  rmdir /S /Q %%s\v15\ipch
+  del %%s\v15\Browse.VC.db
+)
