29c8dfd30b06baf8c44bcbda284478efc75e7680
Rimas Misevičius <rmisev3@gmail.com>
2019-05-14 17:39:24 +0300

    Add more IPv4 parser tests

--- test/test-ipv4.cpp
+++ test/test-ipv4.cpp
@@ -1,7 +1,11 @@
 #include "url.h"
 #include "doctest-main.h"
 
-using str = std::char_traits<char>;
+
+static whatwg::url_result ipv4_parse(const char* szInput, uint32_t& ipv4) {
+    using str = std::char_traits<char>;
+    return whatwg::ipv4_parse(szInput, szInput + str::length(szInput), ipv4);
+};
 
 TEST_CASE("IPv4 parser test with empty input") {
     const char* szEmpty = "";
@@ -15,4 +19,40 @@ TEST_CASE("IPv4 parser test with empty input") {
     // https://url.spec.whatwg.org/#concept-ipv4-parser
     // 6. 1. If part is the empty string, return input.
     CHECK(whatwg::ipv4_parse(szEmpty, szEmpty, ipv4) == whatwg::url_result::False);
+}
+
+TEST_CASE("IPv4 parser test with 127.0.0.1") {
+    uint32_t ipv4;
+
+    ipv4 = 0;
+    CHECK(ipv4_parse("0x7f000001", ipv4) == whatwg::url_result::Ok);
+    CHECK(ipv4 == 0x7f000001);
+
+    ipv4 = 0;
+    CHECK(ipv4_parse("0x7f.0.0.1", ipv4) == whatwg::url_result::Ok);
+    CHECK(ipv4 == 0x7f000001);
+
+    ipv4 = 0;
+    CHECK(ipv4_parse("127.0.0.1", ipv4) == whatwg::url_result::Ok);
+    CHECK(ipv4 == 0x7f000001);
+
+    ipv4 = 0;
+    CHECK(ipv4_parse("127.0.1", ipv4) == whatwg::url_result::Ok);
+    CHECK(ipv4 == 0x7f000001);
+
+    ipv4 = 0;
+    CHECK(ipv4_parse("127.1", ipv4) == whatwg::url_result::Ok);
+    CHECK(ipv4 == 0x7f000001);
+}
+
+TEST_CASE("127.0.0.1 percent encoded") {
+    whatwg::url url;
+
+    // 127.0.0.1 percent encoded
+    CHECK(whatwg::success(url.parse("http://12%37.0.0.1/", nullptr)));
+    CHECK(url.hostname() == "127.0.0.1");
+
+    // 0x7f.0.0.1 percent encoded
+    CHECK(whatwg::success(url.parse("http://%30%78%37%66.0.0.1/", nullptr)));
+    CHECK(url.hostname() == "127.0.0.1");
 }
