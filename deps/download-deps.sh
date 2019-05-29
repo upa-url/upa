420d2610dc314cddd21f283ca577e9e364c09a6a
Rimas Misevičius <rmisev3@gmail.com>
2019-05-29 23:00:27 +0300

    Remove kazuho/picojson as git submodule and add picojson.h as downloadable

--- deps/download-deps.sh
+++ deps/download-deps.sh
@@ -5,3 +5,4 @@
 p="$(dirname "$0")"
 
 curl -fsS -o $p/doctest/doctest.h https://raw.githubusercontent.com/onqtam/doctest/2.3.2/doctest/doctest.h
+curl -fsS -o $p/picojson/picojson.h https://raw.githubusercontent.com/kazuho/picojson/master/picojson.h
