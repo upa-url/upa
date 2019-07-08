763fb8de5721f8e72936d65cee37c00382aec09e
Rimas Misevičius <rmisev3@gmail.com>
2019-07-08 22:25:13 +0300

    Update doctest to 2.3.3

--- deps/download-deps.bat
+++ deps/download-deps.bat
@@ -3,5 +3,5 @@
 REM the directory path of this file
 set p=%~dp0
 
-curl -fsS -o %p%\doctest\doctest.h https://raw.githubusercontent.com/onqtam/doctest/2.3.2/doctest/doctest.h
+curl -fsS -o %p%\doctest\doctest.h https://raw.githubusercontent.com/onqtam/doctest/2.3.3/doctest/doctest.h
 curl -fsS -o %p%\picojson\picojson.h https://raw.githubusercontent.com/kazuho/picojson/master/picojson.h
