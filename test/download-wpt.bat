db870044ce4e2527390c35384027e850cd970547
Rimas Misevičius <rmisev3@gmail.com>
2019-10-18 20:59:27 +0300

    Update web platform tests commit hash
    See:
    https://github.com/web-platform-tests/wpt/pull/19770
    https://github.com/web-platform-tests/wpt/pull/19790

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=8553d30dd590715a63273e90da1951b8b0b37672
+set HASH=43feb7f612fe9160639e09a47933a29834904d69
 
 for %%f in (setters_tests.json toascii.json urltestdata.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=8553d30dd590715a63273e90da1951b8b0b37672
+HASH=43feb7f612fe9160639e09a47933a29834904d69
 
 for f in setters_tests.json toascii.json urltestdata.json
 do
