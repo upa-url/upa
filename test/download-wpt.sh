a24cfa4faf5731978386ae9a208b5a8107167433
Rimas Misevičius <rmisev3@gmail.com>
2020-04-27 21:13:16 +0300

    Update web platform tests: percent encode NULLs in fragments
    See: https://github.com/web-platform-tests/wpt/pull/23256

--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=556afc9ad1a27504be759fb9e62a266a1f653b09
+HASH=efec8204e84d434d80407bb7cf8df37d33cabaa1
 
 for f in setters_tests.json toascii.json urltestdata.json
 do
