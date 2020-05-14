361d2642788c951bc0a991d62ff0d9a1195172a1
Rimas Misevičius <rmisev3@gmail.com>
2020-05-14 19:39:52 +0300

    Update web platform tests: forbid <, >, and ^ in hosts
    https://github.com/web-platform-tests/wpt/commit/9ffe8f26432649d02eb81add2816dd5394f57a8c

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=6d82a3b1c7c79608aa8045666922c79accf917f6
+set HASH=9ffe8f26432649d02eb81add2816dd5394f57a8c
 
 for %%f in (setters_tests.json toascii.json urltestdata.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=6d82a3b1c7c79608aa8045666922c79accf917f6
+HASH=9ffe8f26432649d02eb81add2816dd5394f57a8c
 
 for f in setters_tests.json toascii.json urltestdata.json
 do
