8c8ec161cba293a3796bbb3783e4a30e5832a77f
Rimas Misevičius <rmisev3@gmail.com>
2020-05-14 19:07:47 +0300

    Update web platform tests: more file: URL drive letter tests
    https://github.com/web-platform-tests/wpt/commit/6d82a3b1c7c79608aa8045666922c79accf917f6

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=e9a106175a02a192a7239f42a94235fbe0f0c7a3
+set HASH=6d82a3b1c7c79608aa8045666922c79accf917f6
 
 for %%f in (setters_tests.json toascii.json urltestdata.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=e9a106175a02a192a7239f42a94235fbe0f0c7a3
+HASH=6d82a3b1c7c79608aa8045666922c79accf917f6
 
 for f in setters_tests.json toascii.json urltestdata.json
 do
