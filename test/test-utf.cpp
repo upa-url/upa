65327ebc9fdd02840b9b5424800fa69d2c936376
Rimas Misevičius <rmisev3@gmail.com>
2020-07-19 13:38:58 +0300

    Add some UTF-8 to code point conversion tests

---
+++ test/test-utf.cpp
@@ -0,0 +1,37 @@
+#include "url_utf.h"
+#include "str_arg.h"
+#include "doctest-main.h"
+
+
+using namespace whatwg;
+
+template <typename T>
+static uint32_t first_codepoint(T&& strUtf) {
+    const auto inp = make_str_arg(strUtf);
+    const auto* first = inp.begin();
+    const auto* last = inp.end();
+    uint32_t code_point = 0;
+    url_utf::read_utf_char(first, last, code_point);
+    return code_point;
+}
+
+TEST_CASE("url_utf::read_utf_char with UTF-8") {
+    CHECK(first_codepoint(u8"\u007F") == 0x7F);
+    // U+0080..U+07FF
+    CHECK(first_codepoint(u8"\u0080") == 0x0080);
+    CHECK(first_codepoint(u8"\u07FF") == 0x07FF);
+    // U+0800..U+FFFF except surrogates
+    CHECK(first_codepoint(u8"\u0800") == 0x0800);
+    CHECK(first_codepoint(u8"\uD7FF") == 0xD7FF);
+    CHECK(first_codepoint(u8"\uE000") == 0xE000);
+    CHECK(first_codepoint(u8"\uFFFF") == 0xFFFF);
+    // U+10000..U+10FFFF
+    CHECK(first_codepoint(u8"\U00010000") == 0x10000);
+    CHECK(first_codepoint(u8"\U0010FFFF") == 0x10FFFF);
+}
+
+TEST_CASE("url_utf::read_utf_char with invalid UTF-8") {
+    // must return U+FFFD - REPLACEMENT CHARACTER
+    CHECK(first_codepoint(std::string{ char(0xC2), char('x') }) == 0xFFFD);
+    CHECK(first_codepoint(std::string{ char(0xF0), char(0x90), char('x') }) == 0xFFFD);
+}
