f8a7fa479e001a33cfa86637688011a8a05815a0
Rimas Misevičius <rmisev3@gmail.com>
2020-07-31 18:00:22 +0300

    Improve AFL fuzzing stability: beware of using buffered i/o
    * for data input use POSIX read function
    * use AFL shared memory fuzzing (if afl++ 2.66c and later)
    * on non POSIX system still use C++ stream

--- test/afl-main.cpp
+++ test/afl-main.cpp
@@ -10,12 +10,69 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const char* data, std::size_t size);
 
+#if defined(__AFL_FUZZ_TESTCASE_BUF)
+
+// AFL shared memory fuzzing (afl++ 2.66c and later)
+// https://github.com/AFLplusplus/AFLplusplus/blob/stable/llvm_mode/README.persistent_mode.md
+// https://github.com/AFLplusplus/AFLplusplus/blob/stable/examples/persistent_demo/persistent_demo_new.c
+
+#include <unistd.h>
+
+__AFL_FUZZ_INIT();
+
+int main() {
+    __AFL_INIT();
+
+    // must be after __AFL_INIT:
+    const char* buf = reinterpret_cast<const char*>(__AFL_FUZZ_TESTCASE_BUF);
+
+    while (__AFL_LOOP(5000)) {
+        const std::size_t len = __AFL_FUZZ_TESTCASE_LEN;
+
+        // test
+        LLVMFuzzerTestOneInput(buf, len);
+    }
+    return 0;
+}
+
+#elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+// POSIX, see: https://stackoverflow.com/a/11351171
+
+// AFL doesn't work well with buffered i/o. C++ standard library has only
+// buffered i/o functions. So here is used unbuffered POSIX read function.
+// See "STEP 2" here:
+// https://github.com/google/AFL/blob/master/experimental/persistent_demo/persistent_demo.c
+
+#include <unistd.h>
+
+int main() {
+    std::vector<char> buff;
+    buff.resize(1024 * 1024);
+
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+    __AFL_INIT();
+
+    while (__AFL_LOOP(5000))
+#endif
+    {   // read all from stadard input
+        const std::size_t len = read(0, buff.data(), buff.size());
+
+        // test
+        LLVMFuzzerTestOneInput(buff.data(), len);
+    }
+    return 0;
+}
+
+#else
+
 int main() {
     std::vector<char> buff;
     buff.reserve(1024);
 
     // https://github.com/google/AFL/tree/master/llvm_mode
 #ifdef __AFL_HAVE_MANUAL_CONTROL
+    __AFL_INIT();
+
     while (__AFL_LOOP(5000))
 #endif
     {   // read all from std::cin
@@ -26,3 +83,5 @@ int main() {
     }
     return 0;
 }
+
+#endif
