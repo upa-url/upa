0fbabca19ca8b0d091a7b79e8dc6b5836a3ea84a
Rimas Misevičius <rmisev3@gmail.com>
2020-09-15 20:19:02 +0300

    // fix link

--- src/url_search_params.cpp
+++ src/url_search_params.cpp
@@ -36,7 +36,7 @@ void url_search_params::update() {
 // corresponding entry is '%', then byte must be percent encoded, otherwise -
 // replaced with table value (for example, replace ' ' with '+').
 // The table is based on:
-// https://url.spec.whatwg.org/#concept-urlencoded-byte-serializer
+// https://url.spec.whatwg.org/#concept-urlencoded-serializer
 
 const char url_search_params::kEncByte[0x100] = {
 //   0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
