17ab2eee0c1a21cae25215eb39fa5b48b383392a
Rimas Misevičius <rmisev3@gmail.com>
2021-03-20 19:30:58 +0200

    Update Doctest to 2.4.5

--- deps/download-deps.sh
+++ deps/download-deps.sh
@@ -4,5 +4,5 @@
 # https://stackoverflow.com/q/59895
 p="$(dirname "$0")"
 
-curl -fsS -o $p/doctest/doctest.h https://raw.githubusercontent.com/onqtam/doctest/2.4.4/doctest/doctest.h
+curl -fsS -o $p/doctest/doctest.h https://raw.githubusercontent.com/onqtam/doctest/2.4.5/doctest/doctest.h
 curl -fsS -o $p/picojson/picojson.h https://raw.githubusercontent.com/kazuho/picojson/111c9be5188f7350c2eac9ddaedd8cca3d7bf394/picojson.h
