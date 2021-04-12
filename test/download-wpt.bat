a5e5674f746917e2bc856c9b1a00034c520749cf
Rimas Misevičius <rmisev3@gmail.com>
2021-04-12 19:36:36 +0300

    Update WPT commit hash: additional file URL tests
    https://github.com/web-platform-tests/wpt/commit/1439087f27135b06deb70ffbf43e65ff64ff1ee6

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=5eebfdb1f68059549b3efff380dd190bc6078266
+set HASH=1439087f27135b06deb70ffbf43e65ff64ff1ee6
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=5eebfdb1f68059549b3efff380dd190bc6078266
+HASH=1439087f27135b06deb70ffbf43e65ff64ff1ee6
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json
 do
