269a7d3a58ef07684793f238630f755ea07038be
Rimas Misevičius <rmisev3@gmail.com>
2021-07-08 23:45:36 +0300

    Update WPT commit hash
    Mark more URL test cases with "inputCanBeRelative"
    https://github.com/web-platform-tests/wpt/commit/18bb9ddd69d646a4c260eb3a0ccc0caab63dec7e

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=6315c7757a6675f39262167c8196ce562f2a6778
+set HASH=18bb9ddd69d646a4c260eb3a0ccc0caab63dec7e
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=6315c7757a6675f39262167c8196ce562f2a6778
+HASH=18bb9ddd69d646a4c260eb3a0ccc0caab63dec7e
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json
 do
