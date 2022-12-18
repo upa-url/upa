0532a8f77b64a0e1c90b474f664f151363e33ce9
Rimas Misevičius <rmisev3@gmail.com>
2022-12-18 10:40:24 +0200

    Update WPT commit hash
    * add single dot host test
      https://github.com/web-platform-tests/wpt/commit/91e72f51b7e9142799eeddc7af76d7d9b4b8a57b
    
    * URL: Add test for hostnames which get IDNA-mapped to 'localhost' in
      file URLs
      https://github.com/web-platform-tests/wpt/commit/9ac85890e8452c58ba868d9dee65642d7c93f0c5
    
    * URL: fix invalid test
      https://github.com/web-platform-tests/wpt/commit/2f61a65a06eeced2743fc2de8a57716769c3759c

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=0a187bc16933e67dfb8755935143a6dd5a9e12f2
+set HASH=2f61a65a06eeced2743fc2de8a57716769c3759c
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=0a187bc16933e67dfb8755935143a6dd5a9e12f2
+HASH=2f61a65a06eeced2743fc2de8a57716769c3759c
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json
 do
