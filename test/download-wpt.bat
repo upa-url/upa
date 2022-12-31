9feda49ba1b5a591f41789c309cb0190f6fa8201
Rimas Misevičius <rmisev3@gmail.com>
2022-12-31 11:45:28 +0200

    Update WPT commit hash
    URL: add tests for U+0000 and U+FFFF
    https://github.com/web-platform-tests/wpt/commit/9c1758e6dd44a35c35cde64343d1b7f981110e96

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=2f61a65a06eeced2743fc2de8a57716769c3759c
+set HASH=9c1758e6dd44a35c35cde64343d1b7f981110e96
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=2f61a65a06eeced2743fc2de8a57716769c3759c
+HASH=9c1758e6dd44a35c35cde64343d1b7f981110e96
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json
 do
