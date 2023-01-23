d199a0562efb4fde688345e64ded86f70048942f
Rimas Misevičius <rmisev3@gmail.com>
2023-01-23 17:30:15 +0200

    Update WPT commit hash
    URL: add coverage for IPv6 with percent-encoding
    https://github.com/web-platform-tests/wpt/commit/e6e95cbf31add408cc1adb5b941cade752e729a2

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=3d997a3ff43a545b3d36e12d55478fb264e6d0df
+set HASH=e6e95cbf31add408cc1adb5b941cade752e729a2
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
