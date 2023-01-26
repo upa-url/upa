cbcf3043eccb380cb4bef7486465ac3b02d2f674
Rimas Misevičius <rmisev3@gmail.com>
2023-01-26 17:15:36 +0200

    Update WPT commit hash
    * URL and HTML: correct protocol setter tests
      https://github.com/web-platform-tests/wpt/commit/865a920dd12159c324560f482288094088cba1ce
    
    * URL: fix setters tests
      https://github.com/web-platform-tests/wpt/commit/f1ade799d04b72b0ff75c13e988744c2cd873741

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=e6e95cbf31add408cc1adb5b941cade752e729a2
+set HASH=f1ade799d04b72b0ff75c13e988744c2cd873741
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=e6e95cbf31add408cc1adb5b941cade752e729a2
+HASH=f1ade799d04b72b0ff75c13e988744c2cd873741
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json
 do
