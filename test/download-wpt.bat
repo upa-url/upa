2b7762e1612f71fe1f11de7626d23cefe3b6d4da
Rimas Misevičius <rmisev3@gmail.com>
2023-03-13 17:40:55 +0200

    Update WPT: test U+FFFF in URLSearchParams
    Follows: https://github.com/web-platform-tests/wpt/commit/b0099c748bac89b857e77ba9e132123b7db61693

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=5d5a73183f333f572efd140e043cadf6ea44b87a
+set HASH=b0099c748bac89b857e77ba9e132123b7db61693
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=5d5a73183f333f572efd140e043cadf6ea44b87a
+HASH=b0099c748bac89b857e77ba9e132123b7db61693
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
