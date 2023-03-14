cc8327bacf028064c79bc54199bf623e5ec3b7cd
Rimas Misevičius <rmisev3@gmail.com>
2023-03-14 18:36:31 +0200

    Update WPT: Add more non-special scheme tests
    https://github.com/web-platform-tests/wpt/commit/ebd56fc67d9a0df29606547da6a5da4512acb88e

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=b0099c748bac89b857e77ba9e132123b7db61693
+set HASH=ebd56fc67d9a0df29606547da6a5da4512acb88e
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=b0099c748bac89b857e77ba9e132123b7db61693
+HASH=ebd56fc67d9a0df29606547da6a5da4512acb88e
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
