01b17c73c222f12e966b83f0518e35633fa6e459
Rimas Misevičius <rmisev3@gmail.com>
2023-04-27 22:59:30 +0300

    Update ICU to 73.1 for Windows (AppVeyor, GitHub Actions)

--- install-icu.bat
+++ install-icu.bat
@@ -10,9 +10,9 @@ set VERSION=%2
 
 mkdir %BASE_DIR%
 curl -fsSL -o %BASE_DIR%\icu4c-bin.zip https://github.com/unicode-org/icu/releases/download/release-%VERSION%-1/icu4c-%VERSION%_1-Win64-MSVC2019.zip
-if %VERSION% LEQ 71 (
-  7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\ICU
-) ELSE (
+if %VERSION% equ 72 (
   7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\TMP
   7z x %BASE_DIR%\TMP\*\icu-windows.zip -o%BASE_DIR%\ICU
+) else (
+  7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\ICU
 )
