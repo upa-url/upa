51a581a865526cb0eb492c0a64ac4e457f40fb6e
Rimas Misevičius <rmisev3@gmail.com>
2023-05-25 21:20:25 +0300

    Update WPT: add test of blob: URL with non-opaque origin
    The origin of `blob:` URL is non-opaque when inner URL has `http` or
    `https` scheme. This adds test for `http` scheme.
    
    https://github.com/web-platform-tests/wpt/commit/f415bd4b89e69e270d98a0caaac7f36fde99408d

--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=fbd7ee145c0bd0158ae30f3a2fbf3c54263da19e
+HASH=f415bd4b89e69e270d98a0caaac7f36fde99408d
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
