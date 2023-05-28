833ddfb7f78d2d781f029e0733c624acc96f3a29
Rimas Misevičius <rmisev3@gmail.com>
2023-05-28 16:56:34 +0300

    Update WPT
    * URL: add blob: URL with percent-encoded URL in path
      https://github.com/web-platform-tests/wpt/commit/17b425e60c2b071d35b14b282f96181e7be231b2
    
    * Revert "URL: ensure surrogates end up as U+FFFD"
      https://github.com/web-platform-tests/wpt/commit/b7e75b1f1a2b0f74e0e7bffb3005f133871c6aa6

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=f415bd4b89e69e270d98a0caaac7f36fde99408d
+set HASH=b7e75b1f1a2b0f74e0e7bffb3005f133871c6aa6
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=f415bd4b89e69e270d98a0caaac7f36fde99408d
+HASH=b7e75b1f1a2b0f74e0e7bffb3005f133871c6aa6
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
