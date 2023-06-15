1f6723752cbf64af5a393066727e0a98e1cf8cd1
Rimas Misevičius <rmisev3@gmail.com>
2023-06-15 21:36:11 +0300

    Update ICU to 73.2 for Windows (AppVeyor, GitHub Actions)

--- install-icu.bat
+++ install-icu.bat
@@ -1,18 +1,18 @@
 @echo off
 
-if "%2" == "" (
-  echo install-icu ^<base dir^> ^<version^>
+if "%3" == "" (
+  echo install-icu ^<base dir^> ^<version major^> ^<version minor^>
   goto :eof
 )
 
 set BASE_DIR=%1
-set VERSION=%2
+set VER_MAJOR=%2
+set VER_MINOR=%3
 
 mkdir %BASE_DIR%
-curl -fsSL -o %BASE_DIR%\icu4c-bin.zip https://github.com/unicode-org/icu/releases/download/release-%VERSION%-1/icu4c-%VERSION%_1-Win64-MSVC2019.zip
-if %VERSION% equ 72 (
-  7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\TMP
+curl -fsSL -o %BASE_DIR%\icu4c-bin.zip https://github.com/unicode-org/icu/releases/download/release-%VER_MAJOR%-%VER_MINOR%/icu4c-%VER_MAJOR%_%VER_MINOR%-Win64-MSVC2019.zip
+7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\ICU
+if not exist %BASE_DIR%\ICU\bin64 (
+  rename %BASE_DIR%\ICU TMP
   7z x %BASE_DIR%\TMP\*\icu-windows.zip -o%BASE_DIR%\ICU
-) else (
-  7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\ICU
 )
