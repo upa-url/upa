cdf25d8f859085857fb1cc4e9c1bac6c2c272ee7
Rimas Misevičius <rmisev3@gmail.com>
2023-09-03 13:37:43 +0300

    Update amalgamate config files, add amalgamate scripts

---
+++ tools/amalgamate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# the directory path of this file
+# https://stackoverflow.com/q/59895
+p="$(dirname "$0")"
+
+# CD to repository root folder
+cd $p/..
+
+# Amalgamate
+python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-cpp.json -s . -p tools/amalgamate/config-cpp.prologue
+python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
