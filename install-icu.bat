ec4e6f70fccd1d48a47ca2d8030f0124a0e46e0c
Rimas Misevičius <rmisev3@gmail.com>
2023-11-01 19:06:22 +0200

    Update ICU to 74.1 for Windows (AppVeyor, GitHub Actions)
    Improve the install-icu.bat script to support more ICU versions.

--- install-icu.bat
+++ install-icu.bat
@@ -2,15 +2,24 @@
 
 if "%3" == "" (
   echo install-icu ^<base dir^> ^<version major^> ^<version minor^>
-  goto :eof
+  exit /b 1
 )
 
 set BASE_DIR=%1
 set VER_MAJOR=%2
 set VER_MINOR=%3
 
+if %VER_MAJOR% lss 59 (
+  echo Only ICU version 59.1 or later can be installed.
+  exit /b 1
+)
+set MSVC=MSVC2015
+if %VER_MAJOR% geq 61 (set MSVC=MSVC2017)
+if %VER_MAJOR% geq 68 (set MSVC=MSVC2019)
+if %VER_MAJOR% geq 74 (set MSVC=MSVC2022)
+
 mkdir %BASE_DIR%
-curl -fsSL -o %BASE_DIR%\icu4c-bin.zip https://github.com/unicode-org/icu/releases/download/release-%VER_MAJOR%-%VER_MINOR%/icu4c-%VER_MAJOR%_%VER_MINOR%-Win64-MSVC2019.zip
+curl -fsSL -o %BASE_DIR%\icu4c-bin.zip https://github.com/unicode-org/icu/releases/download/release-%VER_MAJOR%-%VER_MINOR%/icu4c-%VER_MAJOR%_%VER_MINOR%-Win64-%MSVC%.zip
 7z x %BASE_DIR%\icu4c-bin.zip -o%BASE_DIR%\ICU
 if not exist %BASE_DIR%\ICU\bin64 (
   rename %BASE_DIR%\ICU TMP
