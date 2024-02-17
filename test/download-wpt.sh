a2c810d4ba2e33efca2c1c5f8e5a552fa2eb3464
Rimas Misevičius <rmisev3@gmail.com>
2024-02-17 09:57:48 +0200

    Update WPT: backslash after authority for special URLs
    https://github.com/web-platform-tests/wpt/commit/ece2f8c3e878bb2a345eee11313454d24dd1a3d0

--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=07f8002f372581f51698430cb51466e8376b7244
+HASH=ece2f8c3e878bb2a345eee11313454d24dd1a3d0
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
