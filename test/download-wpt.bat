9899268580674b2e2f4856ee5891c4d97c2ebe77
Rimas Misevičius <rmisev3@gmail.com>
2024-04-06 19:45:36 +0300

    Update WPT: add parsing of <file:.> URL tests
    https://github.com/web-platform-tests/wpt/commit/1997461a32bb88a052cf007e58c33a3a6d5041f9

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=ece2f8c3e878bb2a345eee11313454d24dd1a3d0
+set HASH=1997461a32bb88a052cf007e58c33a3a6d5041f9
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=ece2f8c3e878bb2a345eee11313454d24dd1a3d0
+HASH=1997461a32bb88a052cf007e58c33a3a6d5041f9
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
