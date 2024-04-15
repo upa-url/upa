b9f1ca3f2814afd122d59f1de0c43b520a2ce177
Rimas Misevičius <rmisev3@gmail.com>
2024-04-15 17:27:52 +0300

    Update WPT: some additional host setter tests
    https://github.com/web-platform-tests/wpt/commit/12b90e170fe7fea5429d6e5aa4d7612cdf7ed805

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=1997461a32bb88a052cf007e58c33a3a6d5041f9
+set HASH=12b90e170fe7fea5429d6e5aa4d7612cdf7ed805
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=1997461a32bb88a052cf007e58c33a3a6d5041f9
+HASH=12b90e170fe7fea5429d6e5aa4d7612cdf7ed805
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
