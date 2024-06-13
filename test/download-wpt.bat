e28f9c53fefafe98ac575fbed523c48b9375cbf8
Rimas Misevičius <rmisev3@gmail.com>
2024-06-13 17:28:11 +0300

    Update WPT: add tests for File URLs with '|' characters
    See: https://github.com/web-platform-tests/wpt/commit/7f369fef2b6f740a0738510331274bf2cbf7b509

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=12b90e170fe7fea5429d6e5aa4d7612cdf7ed805
+set HASH=7f369fef2b6f740a0738510331274bf2cbf7b509
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=12b90e170fe7fea5429d6e5aa4d7612cdf7ed805
+HASH=7f369fef2b6f740a0738510331274bf2cbf7b509
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
