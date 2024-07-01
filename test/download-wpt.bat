8c29f98732d2af46958e8bc6a483f407bf7edff5
Rimas Misevičius <rmisev3@gmail.com>
2024-07-01 17:21:42 +0300

    Update WPT: add NFC normalization tests to toascii.json
    See: https://github.com/web-platform-tests/wpt/commit/6a39784534e118acc102cf5252c542378831401d

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=7f369fef2b6f740a0738510331274bf2cbf7b509
+set HASH=6a39784534e118acc102cf5252c542378831401d
 
 for %%f in (setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=7f369fef2b6f740a0738510331274bf2cbf7b509
+HASH=6a39784534e118acc102cf5252c542378831401d
 
 for f in setters_tests.json toascii.json urltestdata.json percent-encoding.json IdnaTestV2.json
 do
