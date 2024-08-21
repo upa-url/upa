1261b65a8b04a3af6a8bfafb88270aeaae40640e
Rimas Misevičius <rmisev3@gmail.com>
2024-08-21 10:24:13 +0300

    Update WPT: Add WPT URL test cases for non-special schemes

--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=2b9af57f5f61e06e93a0caff5256d2c435b5c468
+HASH=08519e73d164a716152176a2a64417f4d58e5ea3
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json
 do
