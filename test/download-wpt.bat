b7fdb0e0b7572f73cf6a21c373a1a394fbfba230
Rimas Misevičius <rmisev3@gmail.com>
2024-08-21 10:42:53 +0300

    Update WPT: Add WPT URL test cases for non-special schemes (#62)
    See: https://github.com/web-platform-tests/wpt/commit/08519e73d164a716152176a2a64417f4d58e5ea3

--- test/download-wpt.bat
+++ test/download-wpt.bat
@@ -8,7 +8,7 @@ REM
 REM 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 REM 2. Find "Latest commit" text and click link next to it.
 REM 3. Copy hash from URL
-set HASH=2b9af57f5f61e06e93a0caff5256d2c435b5c468
+set HASH=08519e73d164a716152176a2a64417f4d58e5ea3
 
 for %%f in (setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
