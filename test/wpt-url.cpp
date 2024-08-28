f8237d5fbd2da9d1c6b45117dba7d4b5dea4144c
Rimas Misevičius <rmisev3@gmail.com>
2024-08-28 22:55:52 +0300

    Fix wpt-url.cpp compilation with old Clang (4.0, 5.0)

--- test/wpt-url.cpp
+++ test/wpt-url.cpp
@@ -78,7 +78,7 @@ int test_from_file(RunTests run_tests, const char* file_name)
 class string_or_null {
 public:
     // construct
-    string_or_null() noexcept = default;
+    string_or_null() UPA_NOEXCEPT_17 = default;
     string_or_null(const string_or_null&) = default;
     string_or_null(string_or_null&&) noexcept = default;
     string_or_null(const picojson::value& json_value)
@@ -91,7 +91,7 @@ public:
 
     // assign
     string_or_null& operator=(const string_or_null&) = default;
-    string_or_null& operator=(string_or_null&&) noexcept = default;
+    string_or_null& operator=(string_or_null&&) UPA_NOEXCEPT_17 = default;
 
     // has value?
     explicit operator bool() const noexcept {
