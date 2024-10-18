cccf0332268047a3e296b0a0c93499b0c6d75e14
Rimas Misevičius <rmisev3@gmail.com>
2024-10-18 18:05:36 +0300

    Update amalgamate.sh/.bat scripts
    After this change, these scripts will copy url_for_*.h files to the
    single_include/upa directory because they can be used with amalgamated
    url.h

--- tools/amalgamate.bat
+++ tools/amalgamate.bat
@@ -10,3 +10,6 @@ cd %p%..
 REM Amalgamate
 python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-cpp.json -s . -p tools/amalgamate/config-cpp.prologue --no-duplicates
 python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
+
+REM Copy url_for_*.h files
+copy /y include\upa\url_for_*.h single_include\upa
--- tools/amalgamate.sh
+++ tools/amalgamate.sh
@@ -10,3 +10,6 @@ cd $p/..
 # Amalgamate
 python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-cpp.json -s . -p tools/amalgamate/config-cpp.prologue --no-duplicates
 python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
+
+# Copy url_for_*.h files
+cp -p include/upa/url_for_*.h single_include/upa
