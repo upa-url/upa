73cca098f250fda4c6e0d6cb06cc6627547f6867
Rimas Misevičius <rmisev3@gmail.com>
2024-10-18 19:03:20 +0300

    Update WPT
    Fix a regression on non-special URL, like "git:\\opaque\\path"
    https://github.com/web-platform-tests/wpt/commit/66adfd2a15d0500978448194cc0333b0906b61dc

--- test/download-wpt.sh
+++ test/download-wpt.sh
@@ -9,7 +9,7 @@ p="$(dirname "$0")"
 # 1. Go to https://github.com/web-platform-tests/wpt/tree/master/url
 # 2. Find "Latest commit" text and click link next to it.
 # 3. Copy hash from URL
-HASH=08519e73d164a716152176a2a64417f4d58e5ea3
+HASH=66adfd2a15d0500978448194cc0333b0906b61dc
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json
 do
