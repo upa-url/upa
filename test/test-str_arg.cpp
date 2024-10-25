a44e11efc5250742f8dda34b981f9811e76ac282
Rimas Misevičius <rmisev3@gmail.com>
2024-10-25 22:03:31 +0300

    Check in str_arg_char<StrT> if StrT has size() member
    Before it was checked StrT has length() member.
    
    This allows other containers (e.g. std::vector) to be used as input.

--- test/test-str_arg.cpp
+++ test/test-str_arg.cpp
@@ -9,6 +9,8 @@
 
 #include "upa/str_arg.h"
 //#include "doctest-main.h"
+#include <array>
+#include <vector>
 
 
 // Function to test
@@ -85,6 +87,10 @@ inline void test_char() {
     procfn(upa::str_arg{ ptr, ptr + N });
     procfn(upa::str_arg{ cptr, cptr + N });
     procfn(upa::str_arg{ vptr, vptr + N });
+
+    // has data() and size() members
+    procfn(std::array<CharT, N>{ '1', '2', '3'});
+    procfn(std::vector<CharT>{ '1', '2', '3'});
 
     // std::basic_string
     const std::basic_string<CharT> str{ arr };
