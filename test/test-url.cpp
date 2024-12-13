rmisev/url_whatwg

Add url_from_file_path function with std::filesystem::path parameter

test/test-url.cpp
@@ -369,4 +369,15 @@ TEST_CASE("url_from_file_path") {
         CHECK_THROWS_AS(whatwg::url_from_file_path("\\\\?\\Volume{b75e2c83-0000-0000-0000-602f00000000}\\Test\\Foo.txt"), whatwg::url_error);
         CHECK_THROWS_AS(whatwg::url_from_file_path("\\\\.\\Volume{b75e2c83-0000-0000-0000-602f00000000}\\Test\\Foo.txt"), whatwg::url_error);
     }
+#ifdef WHATWG_URL_USE_FILESYSTEM
+    SUBCASE("std::filesystem::path") {
+# if _WIN32
+        std::filesystem::path path{ "c:\\dir\\file.txt" };
+        CHECK(whatwg::url_from_file_path(path).href() == "file:///c:/dir/file.txt");
+# else
+        std::filesystem::path path{ "/dir/file.txt" };
+        CHECK(whatwg::url_from_file_path(path).href() == "file:///dir/file.txt");
+# endif
+    }
+#endif
 }

upa-url/upa

test/test-url.cpp
@@ -731,6 +731,15 @@ TEST_CASE("url_from_file_path") {
         CHECK(upa::url_from_file_path("/", upa::file_path_format::native).href() == "file:///");
 #endif
     }
+    SUBCASE("std::filesystem::path") {
+#ifdef _WIN32
+        std::filesystem::path path{ "c:\\dir\\file.txt" };
+        CHECK(upa::url_from_file_path(path).href() == "file:///c:/dir/file.txt");
+#else
+        std::filesystem::path path{ "/dir/file.txt" };
+        CHECK(upa::url_from_file_path(path).href() == "file:///dir/file.txt");
+#endif
+    }
 }
 
 TEST_CASE("path_from_file_url") {