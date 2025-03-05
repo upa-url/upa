63444b28f132086fdae93ee93e141912592f65dd
Rimas Misevičius <rmisev3@gmail.com>
2025-03-05 23:34:28 +0200

    Simplify test_idna_v2 function
    Remove no longer needed encodeHostEndingCodePoints lambda.
    
    Follows: https://github.com/web-platform-tests/wpt/commit/d0cd7c05f70f6928234e90ddff90b39ef9c1eebc

--- test/wpt-url.cpp
+++ test/wpt-url.cpp
@@ -1,4 +1,4 @@
-// Copyright 2016-2024 Rimas Misevičius
+// Copyright 2016-2025 Rimas Misevičius
 // Distributed under the BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -223,7 +223,7 @@ void test_host_parser(DataDrivenTest& ddt, const parsed_obj& obj)
     // https://github.com/web-platform-tests/wpt/tree/master/url#toasciijson
     // https://github.com/web-platform-tests/wpt/pull/5976
     static const auto make_url = [](const std::string& host)->std::string {
-        std::string str_url("http://");
+        std::string str_url("https://");
         str_url += host;
         str_url += "/x";
         return str_url;
@@ -289,16 +289,10 @@ void test_idna_v2(DataDrivenTest& ddt, const parsed_obj& obj)
     // https://github.com/web-platform-tests/wpt/tree/master/url#toasciijson
     // https://github.com/web-platform-tests/wpt/pull/5976
     static const auto make_url = [](const std::string& host) -> std::string {
-        std::string str_url("http://");
+        std::string str_url("https://");
         str_url += host;
         str_url += "/x";
         return str_url;
-    };
-
-    static const auto encodeHostEndingCodePoints = [](const std::string& input) -> std::string {
-        if (input.find_first_of(":/?#\\") != input.npos)
-            return encodeURIComponent(input);
-        return input;
     };
 
     // "input" and "output" are mandatory
@@ -315,7 +309,7 @@ void test_idna_v2(DataDrivenTest& ddt, const parsed_obj& obj)
         str_case += " " + *obj.at("comment");
 
     ddt.test_case(str_case, [&](DataDrivenTest::TestCase& tc) {
-        const std::string input_url(make_url(encodeHostEndingCodePoints(*input)));
+        const std::string input_url(make_url(*input));
 
         upa::url url;
         const bool parse_success = upa::success(url.parse(input_url));
