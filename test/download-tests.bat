18d5670ab0e0ac7f4ab538a429e0b0e6c062332d
Rimas Misevičius <rmisev3@gmail.com>
2025-03-06 23:26:07 +0200

    Add U+005E (^) to the path percent-encode set
    Follows: https://github.com/whatwg/url/commit/9bc33c39d4a6cd6a936ea7620b5a69f606ec0d4c
    
    Tests: https://github.com/web-platform-tests/wpt/commit/d2c39364360cfa39ce900f643f55ca6ef7b33093

--- test/download-tests.bat
+++ test/download-tests.bat
@@ -7,7 +7,7 @@ REM Commit hash of web-platform-tests (wpt)
 REM
 REM Run tools\update-wpt.py to update to the latest commit hash from
 REM https://github.com/web-platform-tests/wpt/tree/master/url
-set HASH=7c9c41aedbcd49bf96fb9e35a133293b3977b83d
+set HASH=d2c39364360cfa39ce900f643f55ca6ef7b33093
 
 for %%f in (setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-tests.sh
+++ test/download-tests.sh
@@ -8,7 +8,7 @@ p="$(dirname "$0")"
 #
 # Run tools/update-wpt.py to update to the latest commit hash from
 # https://github.com/web-platform-tests/wpt/tree/master/url
-HASH=7c9c41aedbcd49bf96fb9e35a133293b3977b83d
+HASH=d2c39364360cfa39ce900f643f55ca6ef7b33093
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json
 do
