7d9073080431f1bb666f866037471809f3d8b49a
Rimas Misevičius <rmisev3@gmail.com>
2025-03-18 22:03:21 +0200

    Update WPT: add even more host/hostname setter tests
    https://github.com/web-platform-tests/wpt/commit/9504a83e014b6b26d6c09eaee89c6b83cb7410c2

--- test/download-tests.sh
+++ test/download-tests.sh
@@ -8,7 +8,7 @@ p="$(dirname "$0")"
 #
 # Run tools/update-wpt.py to update to the latest commit hash from
 # https://github.com/web-platform-tests/wpt/tree/master/url
-HASH=072413fba2fef3c16877673af78215174ca8f7c2
+HASH=9504a83e014b6b26d6c09eaee89c6b83cb7410c2
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json
 do
