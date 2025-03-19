f86bf0e431a4ab629cfa31c66942c4410125f610
Rimas Misevičius <rmisev3@gmail.com>
2025-03-19 22:30:30 +0200

    Prepare public_suffix_list.cpp,.h for single_include\upa

--- tools/amalgamate.bat
+++ tools/amalgamate.bat
@@ -11,5 +11,8 @@ REM Amalgamate
 python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-cpp.json -s . -p tools/amalgamate/config-cpp.prologue --no-duplicates
 python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
 
-REM Copy url_for_*.h files
+python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-psl-cpp.json -s . -p tools/amalgamate/config-psl-cpp.prologue --no-duplicates
+
+REM Copy other header files
+copy /y include\upa\public_suffix_list.h single_include\upa
 copy /y include\upa\url_for_*.h single_include\upa
--- tools/amalgamate.sh
+++ tools/amalgamate.sh
@@ -11,5 +11,8 @@ cd $p/..
 python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-cpp.json -s . -p tools/amalgamate/config-cpp.prologue --no-duplicates
 python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
 
-# Copy url_for_*.h files
+python3 tools/amalgamate/amalgamate.py -c tools/amalgamate/config-psl-cpp.json -s . -p tools/amalgamate/config-psl-cpp.prologue --no-duplicates
+
+# Copy other header files
+cp -p include/upa/public_suffix_list.h single_include/upa
 cp -p include/upa/url_for_*.h single_include/upa
