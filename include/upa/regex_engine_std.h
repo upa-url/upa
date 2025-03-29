a368cc1b1fa679a53695d510998a0ed9068bc598
Rimas Misevičius <rmisev3@gmail.com>
2025-03-29 23:16:32 +0200

    Add templated regex engine support to `urlpattern` class
    * convert `urlpattern` class to a class template with `regex_engine`
      parameter
    
    * add `regex_engine_std` class implementing regex engine using
      `std::regex`
    
    The `std::regex` library is not entirely suitable for URLPattern. It is
    based on an outdated ECMAScript standard, which caused some URLPattern
    tests to fail.
    
    This change allows the use of other regex libraries, which may be more
    appropriate.

---
+++ include/upa/regex_engine_std.h
@@ -0,0 +1,67 @@
+// Copyright 2025 Rimas Misevičius
+// Distributed under the BSD-style license that can be
+// found in the LICENSE file.
+//
+#ifndef UPA_REGEX_ENGINE_STD_H
+#define UPA_REGEX_ENGINE_STD_H
+
+#include <cstddef>
+#include <optional>
+#include <regex>
+#include <string_view>
+
+namespace upa::pattern {
+
+class regex_engine_std {
+public:
+    class result {
+    public:
+        std::size_t size() const {
+            return arr_.size();
+        }
+        std::optional<std::string_view> get(std::size_t ind, std::string_view input) const {
+            const auto& res = arr_[ind];
+            if (res.matched) {
+#ifdef UPA_CPP_20
+                return std::string_view{ res.first, res.second };
+#else
+                return input.substr(arr_.position(ind), arr_.length(ind));
+#endif
+            }
+            return std::nullopt;
+        }
+    private:
+        std::match_results<std::string_view::const_iterator> arr_;
+        friend class regex_engine_std;
+    };
+
+    bool init(std::string_view regex_str, bool ignore_case) {
+        // 4. If options's ignore case is true then set flags to "vi".
+        // 5. Otherwise set flags to "v"
+        // TODO: "v" flag
+        const std::regex::flag_type flag = ignore_case
+            ? (std::regex::ECMAScript | std::regex::icase)
+            : std::regex::ECMAScript;
+        try {
+            re_.assign(regex_str.data(), regex_str.length(), flag);
+            return true;
+        }
+        catch (const std::regex_error&) {
+            return false;
+        }
+    }
+
+    bool exec(std::string_view input, result& res) const {
+        return std::regex_match(input.begin(), input.end(), res.arr_, re_);
+    }
+    bool test(std::string_view input) const {
+        return std::regex_match(input.begin(), input.end(), re_);
+    }
+
+private:
+    std::regex re_;
+};
+
+} // namespace upa::pattern
+
+#endif // UPA_REGEX_ENGINE_STD_H
