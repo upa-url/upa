0ed88f01cc279befd33192f39142b87420db4847
Rimas Misevičius <rmisev3@gmail.com>
2025-04-19 23:32:09 +0300

    Remove unnecessary `inline`

--- include/upa/url_utf.h
+++ include/upa/url_utf.h
@@ -1,4 +1,4 @@
-// Copyright 2016-2024 Rimas Misevičius
+// Copyright 2016-2025 Rimas Misevičius
 // Distributed under the BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -47,7 +47,7 @@ protected:
     static constexpr bool read_code_point(const char32_t*& first, const char32_t* last, uint32_t& code_point) noexcept;
 private:
     // Replacement character (U+FFFD)
-    static inline constexpr std::string_view kReplacementCharUtf8{ "\xEF\xBF\xBD" };
+    static constexpr std::string_view kReplacementCharUtf8{ "\xEF\xBF\xBD" };
 
     // Following two arrays have values from corresponding macros in ICU 74.1 library's
     // include\unicode\utf8.h file.
@@ -56,14 +56,14 @@ private:
     // Each bit indicates whether one lead byte + first trail byte pair starts a valid sequence.
     // Lead byte E0..EF bits 3..0 are used as byte index,
     // first trail byte bits 7..5 are used as bit index into that byte.
-    static inline constexpr uint8_t k_U8_LEAD3_T1_BITS[16] = {
+    static constexpr uint8_t k_U8_LEAD3_T1_BITS[16] = {
         0x20, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x10, 0x30, 0x30
     };
     // Internal bit vector for 4-byte UTF-8 validity check, for use in U8_IS_VALID_LEAD4_AND_T1.
     // Each bit indicates whether one lead byte + first trail byte pair starts a valid sequence.
     // First trail byte bits 7..4 are used as byte index,
     // lead byte F0..F4 bits 2..0 are used as bit index into that byte.
-    static inline constexpr uint8_t k_U8_LEAD4_T1_BITS[16] = {
+    static constexpr uint8_t k_U8_LEAD4_T1_BITS[16] = {
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1E, 0x0F, 0x0F, 0x0F, 0x00, 0x00, 0x00, 0x00
     };
 };
