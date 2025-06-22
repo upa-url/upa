e976e1d426fdddec02361bd5b13565457122d88d
Rimas Misevičius <rmisev3@gmail.com>
2025-06-22 11:27:57 +0300

    Fix build failure for shared library with URL Pattern functionality

--- include/upa/unicode_id.h
+++ include/upa/unicode_id.h
@@ -5,55 +5,12 @@
 #ifndef UPA_UNICODE_ID_H
 #define UPA_UNICODE_ID_H
 
-#include <cstddef>
-#include <cstdint>
+#include "upa/config.h"
 
 namespace upa::pattern::table {
 
-
-// BEGIN-GENERATED
-const std::uint8_t id_start_bit = 0x1;
-const std::uint8_t id_part_bit = 0x10;
-const std::size_t id_bit_shift = 2;
-const char32_t id_bit_mask = 0x3;
-
-const std::size_t id_block_shift = 5;
-const std::uint32_t id_block_mask = 0x1F;
-
-const char32_t id_start_default_start = 0x323B0;
-const std::uint8_t id_start_default_value = 0;
-const char32_t id_part_default_start = 0x323B0;
-const std::uint8_t id_part_default_value = 0;
-const char32_t id_part_spec_from = 0xE0100;
-const char32_t id_part_spec_to = 0xE01EF;
-const std::uint8_t id_part_spec_value = 0x1;
-
-extern const std::uint8_t id_data[];
-extern const std::uint8_t id_index[];
-// END-GENERATED
-
-
-inline bool in_table(std::uint8_t bit, char32_t cp) noexcept {
-    const auto ind = cp >> id_bit_shift;
-    const auto item = id_data[(id_index[ind >> id_block_shift] << id_block_shift) | (ind & id_block_mask)];
-    return (item & (bit << (cp & id_bit_mask))) != 0;
-}
-
-inline bool is_identifier_start(char32_t cp) noexcept {
-   if (cp >= id_start_default_start)
-       return id_start_default_value;
-   return in_table(id_start_bit, cp);
-}
-
-inline bool is_identifier_part(char32_t cp) noexcept {
-    if (cp >= id_part_default_start) {
-        if (cp >= id_part_spec_from && cp <= id_part_spec_to)
-            return id_part_spec_value;
-        return id_part_default_value;
-    }
-    return in_table(id_part_bit, cp);
-}
-
+UPA_API bool is_identifier_start(char32_t cp) noexcept;
+UPA_API bool is_identifier_part(char32_t cp) noexcept;
 
 } // namespace upa::pattern::table
 
