b4c1332cb486eae582b3f8284b77050f1f6c947b
Rimas Misevičius <rmisev3@gmail.com>
2025-06-30 22:44:48 +0300

    Add `regex_engine_srell` and use it by default for testing
    The `regex_engine_srell` class depends on SRELL, a std::regex-like
    library. This library is well-suited for URL pattern implementation
    because it conforms to the latest ECMAScript specification.
    
    The library can be found at https://www.akenotsuki.com/misc/srell/en/

--- deps/download-deps.sh
+++ deps/download-deps.sh
@@ -7,3 +7,4 @@ p="$(dirname "$0")"
 curl -fsS -o $p/doctest/doctest.h https://raw.githubusercontent.com/doctest/doctest/v2.4.12/doctest/doctest.h
 curl -fsS -o $p/picojson/picojson.h https://raw.githubusercontent.com/kazuho/picojson/111c9be5188f7350c2eac9ddaedd8cca3d7bf394/picojson.h
 curl -fsS -o $p/ankerl/nanobench.h https://raw.githubusercontent.com/martinus/nanobench/v4.3.11/src/include/nanobench.h
+curl -fsS -o $p/srell/srell.hpp https://raw.githubusercontent.com/upa-url/srell/refs/tags/v4.067/srell-src/single-header/srell.hpp
