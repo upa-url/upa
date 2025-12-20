ca7cd0348bdebe09c30d212d163a39fabb5c53b3
Rimas Misevičius <rmisev3@gmail.com>
2025-12-20 15:42:51 +0200

    Update scripts to amalgamate URLPattern implementation files
    * update scripts and add configuration files for amalgamation
    
    * add tests

--- tools/amalgamate.bat
+++ tools/amalgamate.bat
@@ -13,6 +13,10 @@ python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-h.json -s .
 
 python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-psl-cpp.json -s . -p tools/amalgamate/config-psl-cpp.prologue --no-duplicates
 
+python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-urlpattern-cpp.json -s . -p tools/amalgamate/config-urlpattern-cpp.prologue --no-duplicates
+python tools/amalgamate/amalgamate.py -c tools/amalgamate/config-urlpattern-h.json -s .
+
 REM Copy other header files
 copy /y include\upa\public_suffix_list.h single_include\upa
+copy /y include\upa\regex_engine_*.h single_include\upa
 copy /y include\upa\url_for_*.h single_include\upa
