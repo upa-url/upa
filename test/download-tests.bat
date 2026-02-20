2c94bdb38769c04676fd703b8ebd9bbca1cec771
Rimas Misevičius <rmisev3@gmail.com>
2026-02-20 16:44:07 +0200

    Update URL Pattern tests
    Added WPT for ECMAScript invalid regexp input that should cause
    URLPattern parsing to fail.
    https://github.com/web-platform-tests/wpt/commit/19c50f6e3c10dad312ddab355ab3fefd6cb0fadb

--- test/download-tests.bat
+++ test/download-tests.bat
@@ -15,7 +15,7 @@ for %%f in (setters_tests.json toascii.json urltestdata.json urltestdata-javascr
 
 REM Commit hash of URL Pattern web-platform-tests
 REM https://github.com/web-platform-tests/wpt/tree/master/urlpattern
-set URLP_HASH=a2e15ad40518c30c4e7f649584dbda699a40d531
+set URLP_HASH=19c50f6e3c10dad312ddab355ab3fefd6cb0fadb
 
 for %%f in (urlpatterntestdata.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%URLP_HASH%/urlpattern/resources/%%f
