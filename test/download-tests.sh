4b923dac2e97a1f63d2ec8646b9102dc4639da48
Rimas Misevičius <rmisev3@gmail.com>
2026-03-29 15:02:12 +0300

    Update WPT: coverage for http:/ against http://example.com/
    https://github.com/web-platform-tests/wpt/commit/fddd06acf60ab064597210321a873b0fac93f71f

--- test/download-tests.sh
+++ test/download-tests.sh
@@ -8,7 +8,7 @@ p="$(dirname "$0")"
 #
 # Run tools/update-wpt.py to update to the latest commit hash from
 # https://github.com/web-platform-tests/wpt/tree/master/url
-HASH=fc3e651593fab938433613eb5ad0b4dbd5a4e167
+HASH=fddd06acf60ab064597210321a873b0fac93f71f
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json
 do
