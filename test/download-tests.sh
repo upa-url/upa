56f37c2ecfd390469b893d3a121f7a84b49aa873
Rimas Misevičius <rmisev3@gmail.com>
2026-04-30 16:21:24 +0300

    Update WPT: Reject IPv4 host numbers whose value exceeds 0xFFFFFFFF
    https://github.com/web-platform-tests/wpt/commit/258f285de043b79e44324228c0fd800b38d21879

--- test/download-tests.sh
+++ test/download-tests.sh
@@ -8,7 +8,7 @@ p="$(dirname "$0")"
 #
 # Run tools/update-wpt.py to update to the latest commit hash from
 # https://github.com/web-platform-tests/wpt/tree/master/url
-HASH=d61231d9ceef372cc66d64ac19c467c2506a8814
+HASH=258f285de043b79e44324228c0fd800b38d21879
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json
 do
