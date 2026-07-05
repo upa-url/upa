5f8641b8c33f004dbf2da8e3542892a0fc4064af
Rimas Misevičius <rmisev3@gmail.com>
2026-07-05 18:34:35 +0300

    Update WPT: test astral code point followed by a trailing character in userinfo
    https://github.com/web-platform-tests/wpt/commit/181476aa16e8b28a07698bef3a0275fa53dd22e5

--- test/download-tests.bat
+++ test/download-tests.bat
@@ -7,7 +7,7 @@ REM Commit hash of URL web-platform-tests (wpt)
 REM
 REM Run tools\update-wpt.py to update to the latest commit hash from
 REM https://github.com/web-platform-tests/wpt/tree/master/url
-set HASH=b63305b743ed9ce2725d4ae09c5c8f0c40d8e6e1
+set HASH=181476aa16e8b28a07698bef3a0275fa53dd22e5
 
 for %%f in (setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json) do (
   curl -fsS -o %p%\wpt\%%f https://raw.githubusercontent.com/web-platform-tests/wpt/%HASH%/url/resources/%%f
--- test/download-tests.sh
+++ test/download-tests.sh
@@ -8,7 +8,7 @@ p="$(dirname "$0")"
 #
 # Run tools/update-wpt.py to update to the latest commit hash from
 # https://github.com/web-platform-tests/wpt/tree/master/url
-HASH=b63305b743ed9ce2725d4ae09c5c8f0c40d8e6e1
+HASH=181476aa16e8b28a07698bef3a0275fa53dd22e5
 
 for f in setters_tests.json toascii.json urltestdata.json urltestdata-javascript-only.json percent-encoding.json IdnaTestV2.json IdnaTestV2-removed.json
 do
